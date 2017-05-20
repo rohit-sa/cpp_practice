3e1fdb93d1fd0a28f1141c68e17019245aee9b93
rohit-sa <rohitsa.inbox@gmail.com>
2017-05-20 00:56:48 -0700

    2017-5-20 leetcode

--- leetcode/leet_practice.cpp
+++ leetcode/leet_practice.cpp
@@ -4,6 +4,7 @@
 #include <cmath>
 #include <limits.h>
 #include <set>
+#include <algorithm>
 
 using namespace std;
 
@@ -538,12 +539,44 @@ bool isValidSudoku(vector<vector<char>>& board) {
 	return true;
 }
 
-int main(){
-	for(int i = 0; i < 9; i++){
-		for(int j = 0; j < 9; j++){
-			cout << i <<','<< j << "	";
+string countAndSay(int n) {
+	string result = "1";
+	for(int i = 0; i < n-1; i++){
+		int len = result.size();
+		int k = 0,count = 0;
+		string temp = "";
+		for(int j = 0; j < len; j++){
+			if(result.at(j) == result.at(k)){
+				count++;
+			}
+			else{
+				temp = temp + to_string(count) + result.at(k);
+				k = j;
+				count = 1;
+			}
 		}
-		cout << endl;
+		temp = temp + to_string(count) + result.at(k);
+		result = temp;
 	}
+	return result;
+}
+
+int firstMissingPositive(vector<int>& nums) {
+	int len = nums.size();
+	for(int i = 0; i < len; i++){
+		 while(nums[i] > 0 && nums[i] <= len && nums[nums[i] - 1] != nums[i]){
+			swap(nums[i], nums[nums[i] - 1]);
+		}
+	}
+	for(int i = 0; i < len; i++){
+		if(nums[i] != i+1){
+			return i+1;
+		}
+	}
+	return len+1;
+}
+
+int main(){
+	
 	return 0;
 }
